4da168b8018b86151679993d090249bf73a2a5aa
Regis de Toni <regis@mura.com.br>
2025-04-25 12:54:07 -0300

    changed the block for ghub to block windows sendinput

--- mouse_suppression.cpp
+++ mouse_suppression.cpp
@@ -1,87 +1,61 @@
-// mouse_suppression.cpp: This file illustrates how to open up the G HUB BusEnumerator driver
-// and request suppression of all mouse movement macros, for the duration of the process that
-// invokes the IOCTL.
-
+#include <windows.h>
 #include <iostream>
-#include <cstdlib>
-#include <cstdint>
-#include <vector>
-#include <initguid.h>
-#include <Windows.h>
-#include <SetupAPI.h>
 
-// Link against setupapi.lib
-#pragma comment(lib, "setupapi.lib")
+// Function to block mouse input if it's injected
+bool BlockInjectedMouseInput() {
+    INPUT_MESSAGE_SOURCE source;
+    if (GetCurrentInputMessageSource(&source)) {
+        if (source.originId == IMO_INJECTED) {
+            std::cout << "Blocked injected input (originId: " << source.originId << ")" << std::endl;
+            return true;
+        }
+    }
+    else {
+        std::cout << "Failed to get input message source" << std::endl;
+    }
+    return false;
+}
 
-DEFINE_GUID(GUID_LOGI_VIRTUAL_BUS_ENUMERATOR_GP, 0xdfbedcdd, 0x2148, 0x416d, 0x9e, 0x4d, 0xce, 0xcc, 0x24, 0x24, 0x12, 0x8c);
+// Low-level mouse hook procedure
+LRESULT CALLBACK MouseHookProc(int nCode, WPARAM wParam, LPARAM lParam) {
+    if (nCode >= 0) {
+        MSLLHOOKSTRUCT* mouseStruct = (MSLLHOOKSTRUCT*)lParam;
 
+        // Check if the input is injected using the flags
+        if (mouseStruct->flags & LLMHF_INJECTED || mouseStruct->flags & LLMHF_LOWER_IL_INJECTED) {
+            std::cout << "Blocked input based on LLMHF flags" << std::endl;
+            return 1;
+        }
 
-int main()
-{
-    printf("This sample application will enable mouse suppression. As long as this executable is\n");
-    printf("running, no mouse movement will be emulated by the driver.\n");
+        if (BlockInjectedMouseInput()) {
+            return 1;
+        }
 
-    HDEVINFO hardwareDeviceInfo =
-        SetupDiGetClassDevs(&GUID_LOGI_VIRTUAL_BUS_ENUMERATOR_GP, NULL, NULL, DIGCF_PRESENT | DIGCF_DEVICEINTERFACE);
+    }
+    return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
 
-    SP_INTERFACE_DEVICE_DATA deviceInfoData;
-    memset(&deviceInfoData, 0, sizeof(deviceInfoData));
-    deviceInfoData.cbSize = sizeof(deviceInfoData);
+int main() {
+    // Set up the mouse hook
+    HHOOK mouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseHookProc, GetModuleHandle(NULL), 0);
 
-    if (!SetupDiEnumDeviceInterfaces(hardwareDeviceInfo, 0, &GUID_LOGI_VIRTUAL_BUS_ENUMERATOR_GP, 0, &deviceInfoData))
-    {
-        printf("Failed get enumerate device interfaces.\n");
-        SetupDiDestroyDeviceInfoList(hardwareDeviceInfo);
+    if (mouseHook == NULL) {
+        DWORD error = GetLastError();
+        std::cerr << "Failed to set mouse hook. Error code: " << error << std::endl;
         return 1;
     }
 
-    ULONG requiredLength = 0;
-    SetupDiGetInterfaceDeviceDetail(hardwareDeviceInfo, &deviceInfoData, NULL, 0, &requiredLength, NULL);
-    if (0 == requiredLength)
-    {
-        printf("Failed to get interface detail data length.\n");
-        SetupDiDestroyDeviceInfoList(hardwareDeviceInfo);
-        return 2;
+    std::cout << "Mouse input blocking is active. Press Ctrl+C to exit." << std::endl;
+    std::cout << "Debug logging is enabled - you will see details about input events." << std::endl;
+
+    // Message loop
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
 
-    std::vector<uint8_t> deviceDetailBuffer;
-    deviceDetailBuffer.resize(requiredLength, 0x00);
-    PSP_INTERFACE_DEVICE_DETAIL_DATA pDeviceDetailData =
-        reinterpret_cast<PSP_INTERFACE_DEVICE_DETAIL_DATA>(deviceDetailBuffer.data());
-    pDeviceDetailData->cbSize = sizeof(SP_INTERFACE_DEVICE_DETAIL_DATA);
-    if (!SetupDiGetInterfaceDeviceDetail(hardwareDeviceInfo, &deviceInfoData, pDeviceDetailData, requiredLength,
-                                         &requiredLength, NULL))
-    {
-        printf("Failed to get interface detail data.\n");
-        SetupDiDestroyDeviceInfoList(hardwareDeviceInfo);
-        return 3;
-    }
-    SetupDiDestroyDeviceInfoList(hardwareDeviceInfo);
-
-    int c = 0x811;
-
-    DWORD IOCTL_BUSENUM_SUPPRESS_MOUSE = CTL_CODE(FILE_DEVICE_BUS_EXTENDER, c, METHOD_BUFFERED, FILE_ANY_ACCESS);
-
-    HANDLE busEnumerator = CreateFile(pDeviceDetailData->DevicePath, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
-    if (INVALID_HANDLE_VALUE == busEnumerator)
-    {
-        printf("Failed to open up interface.\n");
-        return 4;
-    }
-
-    BOOL ret = DeviceIoControl(busEnumerator, IOCTL_BUSENUM_SUPPRESS_MOUSE, NULL, 0, NULL, 0, NULL, NULL);
-    DWORD error = GetLastError();
-    if(error == 0){
-        printf("%d", c);
-        printf(" GLE: 0x%08x\n", error);
-    }
-
-
-
-    printf("Press any key to exit.\n");
-    getchar();
-
-    CloseHandle(busEnumerator);
-    
+    // Clean up
+    UnhookWindowsHookEx(mouseHook);
     return 0;
 }
